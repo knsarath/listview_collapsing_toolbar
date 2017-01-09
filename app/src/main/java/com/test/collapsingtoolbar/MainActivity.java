e7c62fb9b7ca60313d5c11e41e4001e6ace5f9bf
sarath.kn <sarath.kn@robosoftin.com>
2017-01-09 10:38:03 +0530

    collapsing

---
+++ app/src/main/java/com/test/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,35 @@
+package com.test.collapsingtoolbar;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new MyFragment(), "Category 1");
+        viewPagerAdapter.addFragment(new MyFragment(), "Category 2");
+        viewPagerAdapter.addFragment(new MyFragment(), "Category 3");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+
+}
